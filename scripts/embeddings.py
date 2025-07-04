48c841c98fa87ad12fb2ce5fdaf0a3e8a6c1f136
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-07-04 13:09:21 -0700

    ...

--- scripts/embeddings.py
+++ scripts/embeddings.py
@@ -4,3 +4,13 @@ from sentence_transformers import SentenceTransformer
 
 
 input_path = Path(__file__).parent.parent / "data/processed/ppc_section.json"
+
+with open(input_path, 'r') as f:
+    ppc_sections = json.load(f)
+
+
+print('loading models...')
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+print('creating embeddings...')
