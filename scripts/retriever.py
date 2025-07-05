3330fe406395a14e37a590c7fef023c6f16b1cf9
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-07-05 11:39:09 -0700

    done

--- scripts/retriever.py
+++ scripts/retriever.py
@@ -8,3 +8,10 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 index = faiss.read_index('data/faiss_index.index')
 texts = pickle.load(open('data/section_texts.pkl', 'rb'))
 
+def search_similar_sections(query, top_k = 5):
+    query_embedding = model.encode([query]).astype('float32')
+    distances, indices = index.search(query_embedding, top_k)
+    results = [texts[i] for i in indices[0]]
+    return results
+
+
