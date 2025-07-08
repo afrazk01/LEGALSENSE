f728e76a3852d5894a476836858f849059409561
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-07-08 05:08:27 -0700

    .

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -14,3 +14,12 @@ logging.basicConfig(
 )
 
 
+nlp = spacy.load("en_core_web_sm", disable=["ner","parser"])
+
+
+Structure_pattern = {
+    'chapter': r'(CHAPTER\s+[IVXLCDM]+)\s*\n+([^\n]+)',
+    'section': r'^\s*(\d+[A-Za-z\-]*)[\.\:\)]?\s+(.*?)(?=\n\s*\d+[A-Za-z\-]*[\.\:\)]|\nCHAPTER|\Z)',
+    'subsection': r'^\s*\(([a-z\d]+)\)\s+(.*?)$'
+}
+
