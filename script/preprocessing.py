8d2ed99ca9b1efd2f61b846acbec239ba479ae4f
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-07-28 13:36:40 -0700

    .

--- script/preprocessing.py
+++ script/preprocessing.py
@@ -9,3 +9,11 @@ def text_formatter(text: str) -> str:
     cleaned_text = text.replace("\n", "").strip()
     return cleaned_text
 
+
+def open_and_read_pdf(pdf_path: str) -> list[dict]:
+    doc = fitz.open(pdf_path)
+    pages_and_text = []
+    for page_number, page in tqdm(enumerate(doc)):
+        text = page.get_text()
+        text = text_formatter(text=text)
+        pages_and_text.append({"page_number":})
