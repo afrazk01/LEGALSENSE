f1e586d68d4c379a4a7c0c165a6e152cf4035957
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-07-28 13:29:00 -0700

    .

--- script/preprocessing.py
+++ script/preprocessing.py
@@ -5,3 +5,7 @@ from tqdm.auto import tqdm
 pdf_path = "U:\\LEGALSENSE\\script\\Pakistan Penal Code.pdf"
 
 
+def text_formatter(text: str) -> str:
+    cleaned_text = text.replace("\n", "").strip()
+    return cleaned_text
+
